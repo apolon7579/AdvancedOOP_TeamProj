dcd6da5b7a827f9e9a2060e43f6dd459186d9b9a
Can I? <ljh15952@gmail.com>
2021-09-14 21:49:24 +0900

    Test

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("HI ");
-		System.out.println("Hi im JungHwa");
+		System.out.println("Hi im JungHwa and jun myeng");
 	}
 
 }
