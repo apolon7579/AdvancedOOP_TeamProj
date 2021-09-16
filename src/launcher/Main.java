67201f1cc4d428c12ba67a4ee581acef60c395f2
apolon7579 <apolon7579@gmail.com>
2021-09-16 18:24:01 +0900

    joonmyung

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -3,13 +3,7 @@ package launcher;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("HI ");
-		System.out.println("HI ");
-		System.out.println("Hi im JungHwa and jun myeng");
-		System.out.println("Hi lee sol");
-        System.out.println("Hi dong wan");
-		//源� �옉�룞 �솗�씤
+		System.out.println("Hello World!");		
 	}
 
 }
