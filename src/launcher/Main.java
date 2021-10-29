7f7f319957b581dbb909cb46275fac26aafcbba0
Can I? <ljh15952@gmail.com>
2021-10-29 18:23:28 +0900

    Slidepanel test

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -5,10 +5,10 @@ import config.DependencyManager;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		// 의존성 관리용 클래스 Frame, Controller등의 초기화를 담당하며 화면을 시작시키는역할
 		DependencyManager.managerStart();
-		
+
 	}
 
 }
