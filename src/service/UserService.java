67e0b328c25cb14fadd30973bd065c54eb271eff
aaa <mjk1234progma@gmail.com>
2021-11-03 16:53:03 +0900

    함수 네이밍 변경

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -3,8 +3,8 @@ package service;
 import entity.User;
 
 public interface UserService {
-	public boolean RegistrationByNameAndUserIdAndPassword(String name, String userId, String password);
-	public boolean LoginByUserIdAndPassword(String userId, String password);
-	public boolean UpdateByUserIdAndUser(String userId, User newUser);
-	public boolean DeleteByuserId(String userId);
+	public boolean registrationByNameAndUserIdAndPassword(String name, String userId, String password);
+	public boolean loginByUserIdAndPassword(String userId, String password);
+	public boolean updateByUserIdAndUser(String userId, User newUser);
+	public boolean deleteByUserId(String userId);
 }
