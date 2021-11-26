9a3b3ea47e29252609f6ed23dbece409a1dcab87
minjun <mjk1234progma@gmail.com>
2021-11-26 18:20:29 +0900

    파서 조금 업그레이드(미검증)

--- src/service/CSVParsingService.java
+++ src/service/CSVParsingService.java
@@ -12,14 +12,16 @@ import entity.City;
 import entity.Climate;
 import entity.Language;
 import entity.Media;
+import entity.Race;
+import entity.Religion;
 
 public class CSVParsingService {
 
-	public static List<NationDto> readFile(String path) throws Exception{
+	public static List<NationDto> readFile(String path) throws Exception {
 		List<NationDto> nationList = new LinkedList<>();
 		BufferedReader csv = new BufferedReader(new FileReader(path));
 		if (csv.readLine().split(",").length != 14) {
-			throw new Exception("칼럼이 14개여야 합니다.");
+			throw new Exception("칼럼이 14개인 공공데이터 형식이 아닙니다.");
 		}
 
 		String line = null;
@@ -49,10 +51,10 @@ public class CSVParsingService {
 				col[index++] = ((tmp.equals("")) ? null : tmp);
 			}
 			if (col[0] == null) {
-				throw new Exception("국가 이름이 없는 국가가 존재합니다. 파일을 수정하고 다시 업로드 해주세요");
+				throw new Exception("국가 이름이 없는 행이 존재합니다. 올바른 파일을 다시 업로드 해주세요");
 			} else {
 				nationList.add(new NationDto(-1, col[0], col[1], col[2], climateParser(col[3]), col[4],
-						cityParser(col[5]), null, null, mediaParser(col[8]),
+						cityParser(col[5]), religionParser(col[6]), raceParser(col[7]), mediaParser(col[8]),
 						((col[9] == null) ? null : Double.parseDouble(col[9])), col[10], col[11],
 						languageParser(col[12]), ((col[13] == null) ? null : Integer.parseInt(col[13])), null));
 			}
@@ -61,11 +63,94 @@ public class CSVParsingService {
 		return nationList;
 	}
 
+	private static List<String> ignoreParenthesisInComma(String origin) {
+		if (origin == null)
+			return null;
+		List<String> parsed = Arrays.asList(origin.split(","));
+		List<String> parenthesisIgnored = new ArrayList<>();
+
+		for (int i = 0; i < parsed.size(); i++) {
+			ArrayList<String> subList = new ArrayList<>();
+			if (parsed.get(i).contains("(")) {
+				subList.add(parsed.get(i++));
+				for (; i < parsed.size(); i++) {
+					if (!parsed.get(i).contains(")")) {
+						subList.add(parsed.get(i));
+					} else {
+						break;
+					}
+				}
+				subList.add(parsed.get(i));
+			} else {
+				subList.add(parsed.get(i));
+			}
+
+			parenthesisIgnored.add(String.join(",", subList).trim());
+		}
+		return parenthesisIgnored;
+	}
+
+	private static List<Race> raceParser(String str) {
+		if (str == null)
+			return null;
+		List<String> parsed = ignoreParenthesisInComma(str);
+
+		List<Race> raceList = new ArrayList<>();
+		for (int i = 0; i < parsed.size(); i++) {
+			if (!parsed.get(i).contains("%")) {
+				raceList.add(new Race(-1, -1, parsed.get(i), null));
+			} else {
+
+				StringBuilder builder = new StringBuilder();
+				StringBuilder reversed = new StringBuilder(parsed.get(i)).reverse();
+				int index = reversed.indexOf("%");
+				for (; index < reversed.length(); index++) {
+					char ch = reversed.charAt(index);
+					if ('0' <= ch && ch <= '9' || ch == '.') {
+						builder.insert(0, ch);
+					}else {
+						break;
+					}
+				}
+				raceList.add(new Race(-1, -1, parsed.get(i), Double.parseDouble(builder.toString())));
+			}
+		}
+		return raceList;
+	}
+
+	private static List<Religion> religionParser(String str) {
+		if (str == null)
+			return null;
+		List<String> parsed = ignoreParenthesisInComma(str);
+
+		List<Religion> religionList = new ArrayList<>();
+		for (int i = 0; i < parsed.size(); i++) {
+			if (!parsed.get(i).contains("%")) {
+				religionList.add(new Religion(-1, -1, parsed.get(i), null));
+			} else {
+
+				StringBuilder builder = new StringBuilder();
+				StringBuilder reversed = new StringBuilder(parsed.get(i)).reverse();
+				int index = reversed.indexOf("%");
+				for (; index < reversed.length(); index++) {
+					char ch = reversed.charAt(index);
+					if ('0' <= ch && ch <= '9' || ch == '.') {
+						builder.insert(0, ch);
+					}else {
+						break;
+					}
+				}
+				religionList.add(new Religion(-1, -1, parsed.get(i), Double.parseDouble(builder.toString())));
+			}
+		}
+		return religionList;
+	}
+
 	private static List<Climate> climateParser(String str) {
 		if (str == null) {
 			return null;
 		} else {
-			List<String> parsed = Arrays.asList(str.split(", "));
+			List<String> parsed = ignoreParenthesisInComma(str);
 			return parsed.stream().map(s -> new Climate(-1, -1, s)).toList();
 		}
 	}
@@ -74,7 +159,7 @@ public class CSVParsingService {
 		if (str == null) {
 			return null;
 		} else {
-			List<String> parsed = Arrays.asList(str.split(", "));
+			List<String> parsed = ignoreParenthesisInComma(str);
 			return (parsed.stream().map(s -> new Language(-1, -1, s))).toList();
 		}
 	}
@@ -83,7 +168,7 @@ public class CSVParsingService {
 		if (str == null) {
 			return null;
 		} else {
-			List<String> parsed = Arrays.asList(str.split(", "));
+			List<String> parsed = ignoreParenthesisInComma(str);
 			return parsed.stream().map(s -> new City(-1, -1, s)).toList();
 		}
 	}
@@ -92,7 +177,7 @@ public class CSVParsingService {
 		if (str == null) {
 			return null;
 		} else {
-			List<String> parsed = Arrays.asList(str.split(", "));
+			List<String> parsed = ignoreParenthesisInComma(str);
 			return parsed.stream().map(s -> new Media(-1, -1, s)).toList();
 		}
 	}
